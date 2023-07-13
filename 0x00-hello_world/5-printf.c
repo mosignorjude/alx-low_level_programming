d6b541d848fb5b323cb49d7da21a0493ddbc925b
mosignorjude <mosignorjude2@gmail.com>
2023-07-13 16:59:10 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - where program execution starts and ends
+  * Return: 0 when successful
+  */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,");
+	
+	return(0);
+}
