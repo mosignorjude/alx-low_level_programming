90f28f68256ffa3eec64b2e959eca71d35b8171b
mosignorjude <mosignorjude2@gmail.com>
2023-07-14 16:24:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+
+	return (0);
+}
