f9bf659bb72bfa3b6f5817bd3c873f826182cafc
mosignorjude <mosignorjude2@gmail.com>
2023-07-18 04:00:15 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * main - contains a function that prints character
+  * Return: Returns 0 on success
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
