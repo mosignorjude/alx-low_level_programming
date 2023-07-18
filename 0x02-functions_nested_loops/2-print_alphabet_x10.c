9a0390cf596549ddede047480c32343bde2d39b3
mosignorjude <mosignorjude2@gmail.com>
2023-07-18 16:30:58 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - prints alphabet 10 times
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letters;
+
+	for (i = 0; i < 10; i++)
+	{
+		for(letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}
