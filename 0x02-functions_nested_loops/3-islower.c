f02ea7d89a78dca1943aa6b41a8a36700ab042cf
mosignorjude <mosignorjude2@gmail.com>
2023-07-18 16:52:11 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _islower- prints alphabet 10 times
+  * Return: 1 if lower case otherwise 0
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
