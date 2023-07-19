1dad97c0e5e49d57e9c9bb15ffa0385503a748d9
mosignorjude <mosignorjude2@gmail.com>
2023-07-19 12:32:14 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,6 @@ void print_times_table(int n)
 
 	if (n > 0 && n <= 15)
 	{
-
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -37,7 +36,7 @@ void print_times_table(int n)
 					printf(", %d", result);
 				}
 			}
-			_putchar('\n');
+			printf("\n");
 		}
 	}
 }
