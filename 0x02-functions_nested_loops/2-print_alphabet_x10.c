736422ed773db8c74ecfa248a95cf992cfaf7de2
mosignorjude <mosignorjude2@gmail.com>
2023-07-19 16:09:37 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for(letters = 'a'; letters <= 'z'; letters++)
+		for (letters = 'a'; letters <= 'z'; letters++)
 		{
 			_putchar(letters);
 		}
