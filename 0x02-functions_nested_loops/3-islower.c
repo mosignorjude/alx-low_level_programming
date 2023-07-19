67a4b41c6197d922eed08c6bf77574b8b139e598
mosignorjude <mosignorjude2@gmail.com>
2023-07-18 17:02:48 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
   * _islower- prints alphabet 10 times
   * Return: 1 if lower case otherwise 0
-  * @int c: takes the character parameter
+  * @c: takes the character parameter
   */
 int _islower(int c)
 {
