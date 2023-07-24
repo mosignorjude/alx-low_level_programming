551ddd78498fbda34d277af91abe171e5f111ec6
mosignorjude <mosignorjude2@gmail.com>
2023-07-24 11:03:39 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts- prints a strings
+* @str: the string to be printed
+* Return: void
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
