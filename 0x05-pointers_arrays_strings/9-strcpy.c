849b9f43ba41606e2bae35ae0e8f5d42d1f9953f
mosignorjude <mosignorjude2@gmail.com>
2023-07-25 06:48:56 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,9 +5,22 @@
  * @src: source variable
  * @dest: destination variable
  * Return: void
- */
+*/
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	/*Create a new pointer to hold the destination address*/
+	char *dest_ptr = dest;
+
+	/*Copy characters from src to dest*/
+	while (*src != '\0')
+	{
+		*dest_ptr = *src;
+		src++;
+		*dest_ptr++;
+
+	}
+	/*Add the null terminator to the destination string*/
+	*dest_ptr = '\0';
+
 	return (dest);
 }
