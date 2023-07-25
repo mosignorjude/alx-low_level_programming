6d4c8ac7b4ad210e4935196cd0e3398ae4db99f1
mosignorjude <mosignorjude2@gmail.com>
2023-07-25 06:25:13 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * _strcpy - copies string to a variable
+ * @src: source variable
+ * @dest: destination variable
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
