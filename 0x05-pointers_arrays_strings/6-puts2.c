5281bd967d4fd775e90c6ccae49c2c41fd44f4b4
mosignorjude <mosignorjude2@gmail.com>
2023-07-25 05:00:23 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,11 @@ void puts2(char *str)
 	/*print the string*/
 	for (i = 0; str[i] != 0; i++)
 	{
-		if (str[i] % 2 != 0)
+		if (i % 2 != 0)
 		{
 			continue;
 		}
 		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
