99d7bde0533f9cb7277846217d332cfa2d527aa7
mosignorjude <mosignorjude2@gmail.com>
2023-08-02 18:53:51 -0700

    recursion tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+
+	else
+		_putchar('\n');
 }
+
