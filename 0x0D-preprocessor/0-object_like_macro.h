098fd333c48a03317f41a7a695b708bb9cf10d6d
mosignorjude <mosignorjude2@gmail.com>
2023-08-14 08:42:37 -0700

    preprocessor and macros

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef MACRO_MAIN_H
+#define MACRO_MAIN_H
+#define SIZE 1024
+
+#endif
+
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef MACRO_MAIN_H
+#define MACRO_MAIN_H
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif
