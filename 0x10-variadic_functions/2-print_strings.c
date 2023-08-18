444133909708f24290ac795ace934c568efda874
mosignorjude <mosignorjude2@gmail.com>
2023-08-17 19:38:38 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,13 +10,10 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	char *s;
+6	char *s;
 
 	va_list(container);
-	if (n == 0)
-	{
-		return;
-	}
+
 	va_start(container, n);
 	for (i = 0; i < n; i++)
 	{
