3d782d365b1ea81b9cac9d65a1da43bf20772f7a
mosignorjude <mosignorjude2@gmail.com>
2023-08-24 17:56:29 -0700

    0-print_list.c update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 		printf("[0] (nil)");
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
