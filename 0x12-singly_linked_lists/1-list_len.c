24f4a351c7b078505f45ff81f5b8c3e5eba2e28a
mosignorjude <mosignorjude2@gmail.com>
2023-08-24 18:39:23 -0700

    1-list_len.c update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_list - prints element of a list
+ * list_len - prints element of a list
  * @h: node pointer
  * Return: number of elements
 */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t len = 0;
 
