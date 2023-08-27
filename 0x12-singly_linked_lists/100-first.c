2d5ca49b72c5bbb6ac7e9f5c019c332f50ea723a
mosignorjude <mosignorjude2@gmail.com>
2023-08-27 10:05:27 -0700

    advanced tasks on linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * myFirstFunction - executes before main
+ * Return: void
+*/
+void myFirstFunction(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
